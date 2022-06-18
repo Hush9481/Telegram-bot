4c39f0304b61a0c3fdc23b24f5e1a7903a7c864d
Maks9481 <maks9481702@gmail.com>
2022-06-18 22:43:44 +0300

    update

--- const.js
+++ const.js
@@ -1,3 +1,2 @@
-const commands = '/start-Перезапустити бота /help-Допомога /math-цифри '
-module.exports.commands = commands;
-
+const commands = '/start-Перезапустити бота /help-Допомога /math-цифри'
+module.exports.commands = commands
